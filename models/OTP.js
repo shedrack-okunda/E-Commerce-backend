7dc1edd9eba1c8df69d6ddd5af5b74a57b4a33f4
Shedrack Okunda <okundashedrack@gmail.com>
2024-11-13 11:44:27 +0300

    created models

---
+++ models/OTP.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const otpSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  otp: { type: String, required: true },
+  expiresAt: { type: Date, required: true },
+});
+
+const OTP = mongoose.model("OTP", otpSchema);
+export default OTP;
